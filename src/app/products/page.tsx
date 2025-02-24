f14b1e0e38f22b66230da210a0dedb928ac8c530
willsonbs <willsonbs@gmail.com>
2025-02-24 16:15:26 -0300

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const ProductPage = () => {
    return ( 
    <div className="p-5">
-      <h1 className="">ProductPage</h1>
+      <h1 className="">Product Page</h1>
          <Button className="p-5 m-3">
             Bem vindo
          </Button>

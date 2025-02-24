b1954833ac6ad1845105a9c324c5a97d953b8463
willsonbs <willsonbs@gmail.com>
2025-02-24 16:08:57 -0300

    feat: add shadcn initial setup and component

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+   return ( 
+   <div className="p-5">
+      <h1 className="">ProductPage</h1>
+         <Button className="p-5 m-3">
+            Bem vindo
+         </Button>
+   </div>
+    );
+}
+ 
+export default ProductPage;
